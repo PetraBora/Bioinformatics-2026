fb910e4f7327f0b64f2eed9709d8c6d6f9c506ed
Petra Bora <petrabora@gmail.com>
2026-02-03 16:44:32 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+greeting="Bello" 
+name="Petra"
+echo "$greeting $name"
+echo "This script is helping you learn bash"
+mkdir -p /Users/petrabora/Desktop/bioinformatics/bash_scripts
+cd /Users/petrabora/Desktop
+mv bash_template.txt /Users/petrabora/Desktop/bioinformatics/bash_scripts
+cd /Users/petrabora/Desktop/bioinformatics/bash_scripts
+mv bash_template.txt bash_template.sh
